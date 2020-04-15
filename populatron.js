535e8a8e56caaa4b8ed0af2f824055e987d718b2
softwaredeveloptam <39637317+softwaredeveloptam@users.noreply.github.com>
2020-04-16 08:23:57 +0900

    I finished huzzah D:

--- populatron.js
+++ populatron.js
@@ -1,5 +1,21 @@
 const fs = require("fs");
+const neatCsv = require("neat-csv");
 
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(onFinished) {
+    let totalPop = 0;
+
+    fs.readFile("./cities.csv", async (err, data) => {
+      if (err) {
+        return;
+      }
+      let popdata = await neatCsv(data);
+
+      totalPop = popdata.reduce((accum, current) => {
+        return Number(accum) + Number(current["population"]);
+      }, 0);
+
+      onFinished(totalPop);
+    });
+  },
 };
